49b69b20f48448779c38945e25c368fae384699d
Alexandre Pereira <pereira.alexandre@gmail.com>
2026-06-23 12:24:13 -0300

    Adiciona integração com Brevo

--- functions/lead.ts
+++ functions/lead.ts
@@ -1,24 +1,3 @@
-export async function onRequestPost({ request, env }) {
-  const formData = await request.formData();
-
-  const name = formData.get("name");
-  const email = formData.get("email");
-
-  await fetch("https://api.brevo.com/v3/contacts", {
-    method: "POST",
-    headers: {
-      "api-key": env.BREVO_API_KEY,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-      attributes: {
-        FIRSTNAME: name,
-      },
-      listIds: [Number(env.BREVO_LIST_ID)],
-      updateEnabled: true,
-    }),
-  });
-
-  return Response.redirect(new URL("/obrigado", request.url), 303);
+export async function onRequest() {
+  return new Response("Function funcionando");
 }
